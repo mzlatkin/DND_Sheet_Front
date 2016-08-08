6e47f6291746777f37a222be54b236effb460827
Martin Zlatkin <mzlatkin@elltechnologies.com>
2016-08-07 21:34:02 -0400

    added listrout]

--- js/js/character_attribute_obj.js
+++ js/js/character_attribute_obj.js
@@ -2,4 +2,11 @@ function character_attribute_viewModel(character_attribute_obj)
 {
     var self = this;    
     ko.mapping.fromJS(character_attribute_obj, {}, self);
+
+    self.calculate_modifier = function(){
+    	return 0
+    }
+    self.modifier = ko.observable(self.calculate_modifier());
+
+
 }
