dbc634785a7fb14dddff55f0242a7a652d866965
Martin Zlatkin <mzlatkin@elltechnologies.com>
2016-08-07 22:10:32 -0400

    added listrout]

--- js/js/character_weapon_obj.js
+++ js/js/character_weapon_obj.js
@@ -2,4 +2,18 @@ function character_weapon_viewModel(character_weapon_obj)
 {
     var self = this;    
     ko.mapping.fromJS(character_weapon_obj, {}, self);
+
+    socket.on("update_equiped", function(data) {
+        self.update_equped(data);
+    })
+
+    self.update_equipped = function(data){
+    	console.log(self.pk);
+    	console.log(data);
+    }
+
+    self.post_equipped = function(){
+    	self.equipped(!self.equipped());
+    	socket.emit("post_weapon_equip", self.pk(),self.equipped());
+    }
 }
--- main.js
+++ main.js
@@ -106,4 +106,14 @@ socket.on("connection", function (client) {
           }
         })
     });
+    client.on("post_weapon_equip", function(pk,equipped){
+        request.post('http://192.168.0.23:8000/weapon_association/'+pk,{ form: { 'equipped': equipped } }, function (error, response, body) 
+            {
+                if (!error && response.statusCode == 200) {
+                    console.log(body)
+                }
+            }
+        );
+    });
+
 });
