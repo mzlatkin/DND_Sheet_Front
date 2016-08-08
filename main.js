dda3fdb00592eca4cdef6bb4fa5396a82115cad3
Martin Zlatkin <mzlatkin@elltechnologies.com>
2016-08-07 22:28:41 -0400

    added listrout]

--- main.js
+++ main.js
@@ -114,7 +114,7 @@ socket.on("connection", function (client) {
             json: true,   // <--Very important!!!
             body: {'equipped': equipped}
         }, function (error, response, body){
-            socket.sockets.emit("got_character_feats", body);
+            socket.sockets.emit("update_equiped", body);
         });
     });
 
