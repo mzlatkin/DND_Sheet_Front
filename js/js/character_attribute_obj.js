f81d93306efb4eaf07366585ec5b4da5a80911bb
Martin Zlatkin <mzlatkin@elltechnologies.com>
2016-08-10 19:35:50 -0400

    let's get them attributes

--- js/js/character_attribute_obj.js
+++ js/js/character_attribute_obj.js
@@ -5,6 +5,8 @@ function character_attribute_viewModel(character_attribute_obj)
 
     self.calculate_modifier = function(){
     	console.log(self.rank());
+    	ret = 0
+    	ret = math.floor((self.rank()-10)/2)
     }
     self.modifier = ko.observable(self.calculate_modifier());
 }
