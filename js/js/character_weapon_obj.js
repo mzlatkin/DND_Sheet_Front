6c1cb7dba77d4ce1449ee205eef1528d5d6000b4
Martin Zlatkin <mzlatkin@elltechnologies.com>
2016-08-10 00:21:41 -0400

    testing moving the weapon equip

--- js/js/character_weapon_obj.js
+++ js/js/character_weapon_obj.js
@@ -8,8 +8,6 @@ function character_weapon_viewModel(character_weapon_obj)
     })
 
     self.update_equipped_weapon = function(data){
-    	console.log(self.pk());
-    	console.log(data.pk);
     	if(self.pk() == data.pk)
     	{
     		self.equipped(data.equipped);
@@ -17,7 +15,6 @@ function character_weapon_viewModel(character_weapon_obj)
     }
 
     self.post_equipped = function(){
-    	console.log("hello");
     	self.equipped(!self.equipped());
     	socket.emit("post_weapon_equip", self.pk(),self.equipped());
     }
--- js/js/dashboard_page_obj.js
+++ js/js/dashboard_page_obj.js
@@ -10,16 +10,21 @@ function dashboard_viewModel()
         self.get_all_characters_success(data)
     })
 
-    socket.on("update_equiped", function(data) {
-        self.update_equipped_weapon(data);
-    })
-
     self.join = function(name)
     {
         if (name != "") {
             socket.emit("join", name);
             ready = true;
             self.joined(true);
+        }
+    }
+
+    self.select_character = function(data)
+    {
+        console.log(data);
+        for (var i = 0, i_len = self.characters().length; i < i_len; ++i)
+        {
+            console.log(self.characters[i]);
         }
     }
 
@@ -34,24 +39,6 @@ function dashboard_viewModel()
         self.characters(temp_array);
     }
 
-    
-    self.update_equipped_weapon = function(data)
-    {
-        for (var i = 0, i_len = self.characters().length; i < i_len; ++i)
-        {
-            if (data["character"] == self.characters()[i].pk())
-            {
-                for (var w = 0, w_len = self.characters()[i].weapons().length; w < w_len; ++w)
-                {
-                    if(self.characters()[i].weapons()[w].pk() == data.pk)
-                    {
-                        self.characters()[i].weapons()[w].equipped(data.equipped)
-                    }
-                }
-            }
-        } 
-    }
-    
 	self.start_app = function() 
     {
         console.log("hello!")
